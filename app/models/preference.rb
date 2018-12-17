206f0a6ecdc50d7b18a7598e880e263268bd861d
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-17 01:46:11 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,11 +1,2 @@
 class Preference < ActiveRecord::Base
-
-  def song_sort_asc
-    @songs = Song.all.order(title: :asc)
-  end
-
-  def song_sort_desc
-    @songs = Song.all.order(title: :desc)
-  end
-
 end
